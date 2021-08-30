883b872ca66c969c7d01f168016afb9f0f8d54a5
mike2611 <mpuentes2611@gmail.com>
2021-08-30 17:35:59 -0500

    Add styles to hamburger header

--- app.js
+++ app.js
@@ -2,13 +2,17 @@ const menu=document.querySelector('#hamburger');
 const menuList=document.querySelector(".menu-list")
 const navBar=document.querySelector(".nav-bar")
 const closeBtn=document.querySelector(".delete")
+const mail = document.querySelector("#mail");
+const logo = document.querySelector(".logo");
 
 
 function toggleButton (){
  menuList.classList.toggle('show')
  navBar.classList.toggle('height')
  closeBtn.classList.toggle('d-none');
- menu.classList.toggle('d-none')
+ menu.classList.toggle('d-none');
+ mail.classList.toggle('d-none');
+ logo.classList.toggle('d-none');
 }
 
 menu.addEventListener('click',toggleButton);
