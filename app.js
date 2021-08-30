becbd4fa07207d58a7a519e66638887daabd6aa9
user <you@example.com>
2021-08-31 01:02:50 +0200

    navbar-completed

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
-const menu=document.querySelector('#hamburger');
-const menuList=document.querySelector(".menu-list")
-const navBar=document.querySelector(".nav-bar")
-const closeBtn=document.querySelector(".delete")
-const mail = document.querySelector("#mail");
-const logo = document.querySelector(".logo");
+const menu = document.querySelector('#hamburger');
+const menuList = document.querySelector('.menu-list');
+const navBar = document.querySelector('.nav-bar');
+const closeBtn = document.querySelector('.delete');
+const mail = document.querySelector('#mail');
+const logo = document.querySelector('.logo');
 
-
-function toggleButton (){
- menuList.classList.toggle('show')
- navBar.classList.toggle('height')
- closeBtn.classList.toggle('d-none');
- menu.classList.toggle('d-none');
- mail.classList.toggle('d-none');
- logo.classList.toggle('d-none');
+function toggleButton() {
+  menuList.classList.toggle('show');
+  navBar.classList.toggle('height');
+  closeBtn.classList.toggle('d-none');
+  menu.classList.toggle('d-none');
+  mail.classList.toggle('d-none');
+  logo.classList.toggle('d-none');
 }
 
-menu.addEventListener('click',toggleButton);
-closeBtn.addEventListener('click',toggleButton)
+menu.addEventListener('click', toggleButton);
+closeBtn.addEventListener('click', toggleButton);
