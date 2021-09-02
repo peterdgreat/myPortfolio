31afe6666fda8af1c64ca8feb03b3ad2841da4f5
user <you@example.com>
2021-09-02 23:05:34 +0200

    Gmail Validated To Allow Only Lower Case

--- validation.js
+++
@@ -1,8 +0,0 @@
-function validation() {
-  const email = document.querySelector('#email');
-  const error = document.querySelector('error');
-
-  error = email === email.toLowerCase() ? '' : 'You should write your mail in lower case';
-
-  return error;
-}
